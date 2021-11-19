1a99a9becb7b1c555c1f2c9cb8bd17ebe794a6b6
saraswatikumari <saraswati.kumari_cs19@gla.ac.in>
2021-11-19 18:59:21 +0530

    Initial commit

---
+++ src/Arrr.java
@@ -0,0 +1,24 @@
+public class Arrr {
+        int arr[];
+        int rollno;
+        String name;
+        Arrr()
+        {
+            this.name="saru";
+            this.rollno=42;
+            arr= new int[5];
+        }
+        class arr{
+            public static void main(String[] args) {
+                int marks[] = new int[0];
+                int i;
+                for(i=0;i<5;i++)
+                {
+                    marks[0]=65;
+                    System.out.println(marks[i]);
+                }
+            }
+        }
+    }
+
+
